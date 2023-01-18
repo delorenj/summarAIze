b0bd0b2cf56810f22eb0bb6f09877cfd7f8a3800
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-01-18 16:57:30 -0500

    Got basic grid layout. TODO: Add global example

--- summaraize-app/src/routes/home.tsx
+++ summaraize-app/src/routes/home.tsx
@@ -1,38 +1,74 @@
-import React, { useContext } from 'react'
+import React, {useContext} from 'react'
 
-import { useNavigate } from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Unstable_Grid2'
-import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import GitHubIcon from '@mui/icons-material/GitHub'
-import Link from '@mui/material/Link'
 
-import logoImage from './logo.png'
-
-import { AuthContext } from '../contexts/authContext'
+import {AuthContext} from '../contexts/authContext'
 import ResponsiveAppBar from "../components/AppBar";
+import {Card, CardActions, CardContent, CardMedia} from "@mui/material";
+import Container from "@mui/material/Container";
+import StickyFooter from "../components/StickyFooter";
 
 export const Home = () => {
-  const navigate = useNavigate()
-  const auth = useContext(AuthContext)
+    const navigate = useNavigate()
+    const auth = useContext(AuthContext)
 
-  function signOutClicked() {
-    auth.signOut()
-    navigate('/')
-  }
+    function signOutClicked() {
+        auth.signOut()
+        navigate('/')
+    }
 
-  function changePasswordClicked() {
-    navigate('changepassword')
-  }
+    function changePasswordClicked() {
+        navigate('changepassword')
+    }
 
-  return (
-    <Grid container>
-      <ResponsiveAppBar />
-      <Grid container direction="column" justifyContent="center" alignItems="center">
-        <Box>Hi</Box>
-      </Grid>
-    </Grid>
-  );
+    const cards = [
+        1, 2, 3, 4, 5, 6, 7
+    ];
+
+    return (
+        <>
+            <ResponsiveAppBar/>
+            <main>
+                <Container sx={{py: 8}} maxWidth="md">
+                    <Grid container spacing={4}>
+                        {cards.map((card) => (
+                            <Grid key={card} xs={12} sm={6} md={4}>
+                                <Card
+                                    sx={{height: '100%', display: 'flex', flexDirection: 'column'}}
+                                >
+                                    <CardMedia
+                                        component="img"
+                                        sx={{
+                                            // 16:9
+                                            pt: '10.25%',
+                                        }}
+                                        image="https://source.unsplash.com/random"
+                                        alt="random"
+                                    />
+                                    <CardContent sx={{flexGrow: 1}}>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Heading
+                                        </Typography>
+                                        <Typography>
+                                            This is a media card. You can use this section to describe the
+                                            content.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small">Original</Button>
+                                        <Button size="small">Summary</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+            <StickyFooter/>
+        </>)
+        ;
 }
