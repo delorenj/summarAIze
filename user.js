bf6f7c60bee226fefa17ae74e4c417f0daabbafe
Jarad DeLorenzo <jaradd@gmail.com>
2023-01-20 15:03:58 -0500

    Added vault

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
-//import AWS from "aws-sdk";
+import AWS from "aws-sdk";
 import handler from "./libs/handler-lib";
 
-//const dynamo = new AWS.DynamoDB.DocumentClient();
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
 export const getData = handler(async (event, context) => {
   const userId = event.requestContext.identity.cognitoIdentityId;
@@ -9,32 +9,15 @@ export const getData = handler(async (event, context) => {
   const stage = process.env.STAGE;
   const tableName = `${stage}-books`;
 
-  console.log("tobby", tableName);
-  return {
-    thanks: userId
+  const params = {
+    TableName: tableName,
+    FilterExpression: "(userId = :userId OR userId = :public)",
+    ExpressionAttributeValues: {
+      ":userId": userId,
+      ":public": "public"
+    }
   };
-  // const params = {
-  //   TableName: tableName,
-  //   FilterExpression: "(userId = :userId OR userId = :public)",
-  //   ExpressionAttributeValues: {
-  //     ":userId": userId,
-  //     ":public": "public"
-  //   }
-  // };
-  //
-  // try {
-  //   const result = await dynamo.query(params).promise();
-  //   return {
-  //     statusCode: 200,
-  //     body: JSON.stringify(result.Items)
-  //   };
-  // } catch (err) {
-  //   console.error(err);
-  //   return {
-  //     statusCode: 500,
-  //     body: JSON.stringify({
-  //       error: err.message
-  //     })
-  //   };
-  // }
+
+  return dynamo.query(params).promise();
+
 });
