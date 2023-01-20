6fc31540924662b2ccf36c37a700b9bbdb237a0d
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-01-19 23:19:58 -0500

    fucucucucuck"

--- user.js
+++ user.js
@@ -1,36 +1,40 @@
-import AWS from "aws-sdk";
+//import AWS from "aws-sdk";
 import handler from "./libs/handler-lib";
 
-const dynamo = new AWS.DynamoDB.DocumentClient();
+//const dynamo = new AWS.DynamoDB.DocumentClient();
 
-export const getUserData = handler(async (event, context) => {
+export const getData = handler(async (event, context) => {
   const userId = event.requestContext.identity.cognitoIdentityId;
 
   const stage = process.env.STAGE;
   const tableName = `${stage}-books`;
 
-  const params = {
-    TableName: tableName,
-    FilterExpression: "(userId = :userId OR userId = :public)",
-    ExpressionAttributeValues: {
-      ":userId": userId,
-      ":public": "public"
-    }
+  console.log("tobby", tableName);
+  return {
+    thanks: userId
   };
-
-  try {
-    const result = await dynamo.query(params).promise();
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result.Items)
-    };
-  } catch (err) {
-    console.error(err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: err.message
-      })
-    };
-  }
+  // const params = {
+  //   TableName: tableName,
+  //   FilterExpression: "(userId = :userId OR userId = :public)",
+  //   ExpressionAttributeValues: {
+  //     ":userId": userId,
+  //     ":public": "public"
+  //   }
+  // };
+  //
+  // try {
+  //   const result = await dynamo.query(params).promise();
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify(result.Items)
+  //   };
+  // } catch (err) {
+  //   console.error(err);
+  //   return {
+  //     statusCode: 500,
+  //     body: JSON.stringify({
+  //       error: err.message
+  //     })
+  //   };
+  // }
 });
