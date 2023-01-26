640bf376f77c87459081c28d109c46a7fe27b84b
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-01-26 09:38:50 -0500

    Got s3 caching working. TODO: actually create metadata now

--- book.js
+++ book.js
@@ -1,5 +1,4 @@
 import * as AWS from "aws-sdk";
-import {v4 as uuid} from "uuid";
 import handler from "./libs/handler-lib";
 import { EPub } from 'epub2';
 
@@ -18,10 +17,10 @@ const writeBookToTemp = async (book) => {
 // a function to read the cached files from tmp
 async function readFileFromTemp(url) {
   const file = await fs.readFile(`/tmp/${url}`);
-
+  console.log("readFileFromTemp", url, file);
   return {
     url: url,
-    fileContents: Buffer.from(file),
+    fileContents: file,
   };
 }
 
@@ -50,64 +49,65 @@ async function readFileFromS3Bucket(url) {
 }
 
 // set this defaulted to false, and set to true when files are cached to tmp
-//let filesCached = false;
+let filesCached = false;
 
 export const parseBookMetadata = handler(async (event, context) => {
-  const userId = event.requestContext.authorizer.claims.sub;
+  // const userId = event.requestContext.authorizer.claims.sub;
   const body = JSON.parse(event.body);
   const bookUrl = body.bookUrl;
-
+  let title = '';
+  let c1 = '';
+  let chapters = [];
   try {
-    const correlationId = uuid();
-    const method = "book.parseBookMetadata";
-    const prefix = `${correlationId} - ${method}`;
 
-    console.log(`${prefix} - started`);
-
-    if (false) {
-      console.log(`${prefix} files are cached - read from tmp on Lambda`);
-
+    if (filesCached) {
+      console.log(`files are cached - read from tmp on Lambda`);
       const bookFile = await readFileFromTemp(bookUrl);
       console.log("bookFile", bookFile.fileContents);
-      let book = new EPub(bookFile.fileContents);
+      let book = await EPub.createAsync(bookFile.fileContents);
       console.log('Read from tmp, and EPub created...');
-      await book.ready;
-      console.log("waited for ready");
-      const title = book.metadata.title;
-      console.log(`got title: ${title}`);
-      // const c1 = await book.getChapter(0);
-      return {
-        statusCode: 200,
-        body: {title, userId}
-      };
-
-    } else {
-      console.log(
-        `${prefix} files are not cached - read from s3 bucket and cache in tmp`
-      );
-      const bookData = await readFileFromS3Bucket(bookUrl);
-      await writeBookToTemp(bookData);
-
-      //filesCached = true; // set cached to true
-
-      let book = await EPub.createAsync(bookData.fileContents.Body);
-      console.log('Read from bucket, stored to tmp, and EPub created...');
-      console.log("waited for ready", JSON.stringify(book.metadata));
-      const title = book.metadata.title;
+      title = book.metadata.title;
       console.log(`got title: ${title}`);
       let chapters = [];
       book.flow.forEach(c => {
         chapters.push(c.id);
       });
       console.log(chapters);
-      let c1 = '';
       book.getChapter('main3', (e,c) => {
         c1 = c;
         console.log(c);
       });
       return {
         statusCode: 200,
-        body: {title, c1}
+        body: {title, c1, cached: true}
+      };
+
+    } else {
+      console.log(
+        `files are not cached - read from s3 bucket and cache in tmp`
+      );
+      const bookData = await readFileFromS3Bucket(bookUrl);
+      await writeBookToTemp(bookData);
+
+      filesCached = true; // set cached to true
+
+      let book = await EPub.createAsync(bookData.fileContents.Body);
+      console.log('Read from bucket, stored to tmp, and EPub created...');
+      console.log("waited for ready", JSON.stringify(book.metadata));
+      title = book.metadata.title;
+      console.log(`got title: ${title}`);
+      chapters = [];
+      book.flow.forEach(c => {
+        chapters.push(c.id);
+      });
+      console.log(chapters);
+      book.getChapter('main3', (e,c) => {
+        c1 = c;
+        console.log(c);
+      });
+      return {
+        statusCode: 200,
+        body: {title, c1, cached: false}
       };
     }
   } catch
