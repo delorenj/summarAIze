433505f32be9c9d0680ac4b0fda158c9ef8e63b9
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-01-27 13:56:17 -0500

    Styling upload modal

--- summaraize-app/src/components/UploadModal.tsx
+++ summaraize-app/src/components/UploadModal.tsx
@@ -1,52 +1,51 @@
 import * as React from 'react';
-import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Slide from '@mui/material/Slide';
 import Typography from '@mui/material/Typography';
 import {useUploadContext} from "../contexts/uploadContext";
 import {useNavigate} from "react-router-dom";
-
-const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    transition: 'transform 2s',
-    bgcolor: 'background.paper',
-    border: '0px solid #000',
-    borderRadius: 2,
-    boxShadow: 24,
-    p: 4,
-};
+import Grid from "@mui/material/Unstable_Grid2";
 
 export default function UploadModal() {
     const {uploadDialogOpen} = useUploadContext();
     const navigate = useNavigate();
     return (
-        <div>
-            <Modal
-                aria-labelledby="transition-modal-title"
-                aria-describedby="transition-modal-description"
-                open={uploadDialogOpen}
-                onClose={() => navigate('/')}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                    timeout: 500,
-                }}
-            >
-                <Slide in={uploadDialogOpen} direction="up" mountOnEnter unmountOnExit>
-                    <Box sx={style}>
-                        <Typography id="transition-modal-title" variant="h6" component="h2">
-                            Text in a modal
-                        </Typography>
-                        <Typography id="transition-modal-description" sx={{mt: 2}}>
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                        </Typography>
-                    </Box>
-                </Slide>
-            </Modal>
-        </div>
+        <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={uploadDialogOpen}
+            onClose={() => navigate('/')}
+            closeAfterTransition
+            style={{alignItems: 'center', justifyContent: 'center'}}
+
+        >
+            <Slide in={uploadDialogOpen} direction="up">
+                <Box sx={{
+                    position: 'absolute',
+                    width: '500px',
+                    left: '500px',
+                    top: '200px',
+                    bgcolor: 'background.paper',
+                    border: '0px solid #000',
+                    borderRadius: 2,
+                    boxShadow: 24,
+                    p: 4,
+                }}>
+                    <Grid container>
+                        <Grid xs={12} lg={6} xl={12} sx={{backgroundColor: 'gray'}}>
+                            <Typography id="transition-modal-title" variant="h6" component="h2">
+                                Text in a modal
+                            </Typography>
+                        </Grid>
+                        <Grid xs={12} lg={6} xl={12} sx={{backgroundColor: 'secondary'}}>
+                            <Typography id="transition-modal-description" sx={{mt: 2}}>
+                                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Slide>
+        </Modal>
     );
 }
