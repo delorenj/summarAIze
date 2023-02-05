24a576cf4ae8734c363f17953146df99eb4ed201
Jarad DeLorenzo <jaradd@gmail.com>
2023-02-05 01:41:34 -0500

    Wtffff now textperpage isn't working for txt

--- book.js
+++ book.js
@@ -13,10 +13,10 @@ const getDirName = require('path').dirname;
 
 // a function to write the files to tmp on the lambda
 const writeBookToTemp = async (book) => {
-  console.log(`writing cached books to /tmp`, `/tmp/${book.url}`, book.fileContents);
-  await fs.mkdir(`/tmp/${getDirName(book.url)}`, {recursive: true});
-  await fs.writeFile(`/tmp/${book.url}`, book.fileContents);
-  console.log("Book written to /tmp");
+    console.log(`writing cached books to /tmp`, `/tmp/${book.url}`, book.fileContents);
+    await fs.mkdir(`/tmp/${getDirName(book.url)}`, {recursive: true});
+    await fs.writeFile(`/tmp/${book.url}`, book.fileContents);
+    console.log("Book written to /tmp");
 };
 
 // a function to read the cached files from tmp
@@ -31,287 +31,287 @@ const writeBookToTemp = async (book) => {
 
 // a function to pull the files from an s3 bucket before caching them locally
 async function readFileFromS3Bucket(url) {
-  console.log('readFileFromS3Bucket', url);
+    console.log('readFileFromS3Bucket', url);
 
-  try {
-    const object = await s3
-      .getObject({
-        Key: url,
-        Bucket: 'summaraize-book'
-      })
-      .promise();
+    try {
+        const object = await s3
+            .getObject({
+                Key: url,
+                Bucket: 'summaraize-book'
+            })
+            .promise();
 
-    console.log("Got object!");
-    console.log("Returning: ", object);
-    return {
-      url: url,
-      fileContents: object.Body,
-      id: object.ETag
-    };
+        console.log("Got object!");
+        console.log("Returning: ", object);
+        return {
+            url: url,
+            fileContents: object.Body,
+            id: object.ETag
+        };
 
-  } catch (err) {
-    console.log("Problem getting S3 object:", err);
-  }
+    } catch (err) {
+        console.log("Problem getting S3 object:", err);
+    }
 }
 
 const getBookFromFileSystemOrS3 = async (url) => {
-  // Always get the book from S3 for now
-  // try {
-  //     const book = await readFileFromTemp(url);
-  //     console.log("Book read from /tmp");
-  //     const metadata = await getBookMetadata(book);
-  //     return {
-  //         url,
-  //         fileContents: book.fileContents,
-  //         metadata
-  //     };
-  // } catch (err) {
-  //     console.log("Problem getting book from /tmp:", err);
-  const book = await readFileFromS3Bucket(url);
-  const metadata = await getBookMetadata(book);
-  await writeBookToTemp(book);
-  return {
-    url,
-    fileContents: book.fileContents,
-    metadata,
-    id: book.id
-  };
-  // };
+    // Always get the book from S3 for now
+    // try {
+    //     const book = await readFileFromTemp(url);
+    //     console.log("Book read from /tmp");
+    //     const metadata = await getBookMetadata(book);
+    //     return {
+    //         url,
+    //         fileContents: book.fileContents,
+    //         metadata
+    //     };
+    // } catch (err) {
+    //     console.log("Problem getting book from /tmp:", err);
+    const book = await readFileFromS3Bucket(url);
+    const metadata = await getBookMetadata(book);
+    await writeBookToTemp(book);
+    return {
+        url,
+        fileContents: book.fileContents,
+        metadata,
+        id: book.id
+    };
+    // };
 };
 
 const isEpub = (fileType) => {
-  return fileType && fileType.mime === "application/epub+zip";
+    return fileType && fileType.mime === "application/epub+zip";
 };
 
 const isPdf = (fileType) => {
-  return fileType && fileType.mime === "application/pdf";
+    return fileType && fileType.mime === "application/pdf";
 };
 
 const numberOfWords = (text) => {
-  return text.split(" ").length;
+    return text.split(" ").length;
 };
 
 const stripNewlinesAndCollapseSpaces = (str) => {
-  return str.replace(/(\r\n|\n|\r|\t|\s{2,})/gm, " ").trim();
+    return str.replace(/(\r\n|\n|\r|\t|\s{2,})/gm, " ").trim();
 };
 
 // This method is called by the client to get the book metadata
 const getEpubMetadata = async (book) => {
-  const epub = await EPub.createAsync(book.fileContents);
-  const title = epub.metadata.title;
-  const chapters = [];
-  let chapterCount = 0;
-  for (const chapter of epub.flow) {
-    chapterCount += 1;
-    const rawChapter = await epub.getChapterAsync(chapter.id);
-    const noTagText = stripNewlinesAndCollapseSpaces(striptags(rawChapter));
-    const numWords = numberOfWords(noTagText);
-    const chapterResult = {
-      id: chapter.id,
-      chapter: chapterCount,
-      title: chapter.title,
-      numWords,
-      firstFewWords: noTagText .split(" ").slice(0, 250).join(" ")
-    };
-    chapters.push(chapterResult);
-  }
-  console.log("about to return", chapters, JSON.stringify(chapters), "chapterCount=" + chapterCount);
-  return {title, chapters};
+    const epub = await EPub.createAsync(book.fileContents);
+    const title = epub.metadata.title;
+    const chapters = [];
+    let chapterCount = 0;
+    for (const chapter of epub.flow) {
+        chapterCount += 1;
+        const rawChapter = await epub.getChapterAsync(chapter.id);
+        const noTagText = stripNewlinesAndCollapseSpaces(striptags(rawChapter));
+        const numWords = numberOfWords(noTagText);
+        const chapterResult = {
+            id: chapter.id,
+            chapter: chapterCount,
+            title: chapter.title,
+            numWords,
+            firstFewWords: noTagText.split(" ").slice(0, 250).join(" ")
+        };
+        chapters.push(chapterResult);
+    }
+    console.log("about to return", chapters, JSON.stringify(chapters), "chapterCount=" + chapterCount);
+    return {title, chapters};
 };
 
 const findChapterBreaks = (doc) => {
-    const numPages = doc.textPerPage.length;
-    console.log("Number of pages", numPages);
-    let chapterBreaks = [];
+        const numPages = doc.textPerPage.length;
+        console.log("Number of pages", numPages);
+        let chapterBreaks = [];
 
-    for (let i = 0; i < numPages; i++) {
-      const page = doc.textPerPage[i];
-      const lines = page.text.match(/[^\r\n]+/g);
-      let chapterCount = 0;
-      // Loop through the lines on the page and look for "Chapter" keyword
-      for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
-        const line = lines[lineIndex];
-        if (line.toLowerCase().includes('chapter')) {
-          chapterCount += 1;
-          chapterBreaks.push({
-            page: i,
-            chapter: chapterCount,
-            firstFewWords: lines.slice(lineIndex, lineIndex + 3).join(" ")
-          });
-          break;
+        for (let i = 0; i < numPages; i++) {
+            let consecutiveEmptyLineCount = 0;
+            const page = doc.textPerPage[i];
+            const lines = page.text.match(/[^\r\n]+/g);
+            let chapterCount = 0;
+            // Loop through the lines on the page and look for "Chapter" keyword
+            for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+                const line = lines[lineIndex];
+                if (stripNewlinesAndCollapseSpaces(line) === "") {
+                    consecutiveEmptyLineCount += 1;
+                    if (consecutiveEmptyLineCount > 3) {
+                        chapterCount += 1;
+                        chapterBreaks.push({
+                            page: i,
+                            chapter: chapterCount,
+                            firstFewWords: lines.slice(lineIndex, lineIndex + 3).join(" ")
+                        });
+                        console.log("Found mid-page chapter break", chapterBreaks);
+                        break;
+                    }
+                    continue;
+                } else {
+                    consecutiveEmptyLineCount = 0;
+                }
+                if (line.toLowerCase().includes('chapter')) {
+                    chapterCount += 1;
+                    chapterBreaks.push({
+                        page: i,
+                        chapter: chapterCount,
+                        firstFewWords: lines.slice(lineIndex, lineIndex + 3).join(" ")
+                    });
+                    console.log("Found chapter break", chapterBreaks);
+                    break;
+                }
+            }
         }
-      }
+        if (chapterBreaks.length === 0) {
+            console.log("No chapter breaks found, adding one at the beginning of the book");
+            console.log('first few words', doc.textPerPage[0].text);
+            chapterBreaks.push({
+                page: 1,
+                chapter: 1,
+                firstFewWords: doc.textPerPage[0].text.slice(0, 250).join(" ")
+            });
+        }
+        console.log("Chapter breaks", chapterBreaks);
+        return chapterBreaks;
     }
-    return chapterBreaks;
-  }
 ;
 
 const getPdfMetadata = async (book) => {
-  const doc = await pdf(book.fileContents);
-  const title = doc.info.Title || getTitleFromUrl(book.url) || "Untitled";
-  const chapters = findChapterBreaks(doc);
-  const info = doc.info;
-  const metadata = doc.metadata;
-  console.log("PDF metadata", {title, chapters, info, metadata});
-  console.log("chapters", chapters);
-  return {
-    title,
-    chapters,
-  };
+    const doc = await pdf(book.fileContents);
+    const title = doc.info.Title || getTitleFromUrl(book.url) || "Untitled";
+    const chapters = findChapterBreaks(doc);
+    const info = doc.info;
+    const metadata = doc.metadata;
+    console.log("PDF metadata", {title, chapters, info, metadata});
+    console.log("chapters", chapters);
+    return {
+        title,
+        chapters,
+    };
 };
 
 const getTitleFromUrl = (url) => {
-  return url.split("/").pop().split(".")[0];
+    return url.split("/").pop().split(".")[0];
 };
 
 const getKeyFromUrl = (url) => {
-  return url.split("/").pop();
+    return url.split("/").pop();
 };
 
 const splitStringIntoArray = (str) => {
-  const words = str.split(' ');
-  let wordCount = 0;
-  let result = [];
-  let currentString = "";
-  for (const word of words) {
-    wordCount += 1;
-    if (wordCount <= 500) {
-      currentString += `${word} `;
-    } else {
-      result.push(currentString);
-      currentString = "";
-      wordCount = 0;
-    }
-  }
-  if (currentString) {
-    result.push(currentString);
-  }
-  return result;
-};
-const extractTextFromFileBuffer = async (fileBuffer) => {
-  return new Promise((resolve, reject) => {
-    fs.open(fileBuffer, 'r', (err, fd) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      fs.read(fd, Buffer.alloc(fileBuffer.length), 0, fileBuffer.length, 0, (err, bytesRead, buffer) => {
-        if (err) {
-          reject(err);
-          return;
+    const words = str.split(' ');
+    let wordCount = 0;
+    let result = [];
+    let currentString = "";
+    for (const word of words) {
+        wordCount += 1;
+        if (wordCount <= 500) {
+            currentString += `${word} `;
+        } else {
+            result.push({text: currentString});
+            currentString = "";
+            wordCount = 0;
         }
-
-        fs.close(fd, (err) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          resolve(buffer.toString('utf-8'));
-        });
-      });
-    });
-  });
+    }
+    if (currentString) {
+        result.push({text: currentString});
+    }
+    return result;
 };
 
 const getGenericMetadata = async (book) => {
-  const text = await extractTextFromFileBuffer(book.fileContents);
-  const textPerPage = splitStringIntoArray(text);
-  console.log('textPerPage', textPerPage.length);
-  const paginatedBook = {
-    ...book,
-    ...{textPerPage}
-  };
-  console.log('paginatedBook', paginatedBook);
-  const chapters = findChapterBreaks(paginatedBook);
-  console.log('chapters', chapters);
-  return {
-    title: getTitleFromUrl(book.url),
-    chapters
-  };
+    const text = book.fileContents.toString();
+    const textPerPage = splitStringIntoArray(text);
+    console.log('textPerPage', textPerPage.length);
+    const paginatedBook = {
+        ...book,
+        ...{textPerPage}
+    };
+    const chapters = findChapterBreaks(paginatedBook);
+    return {
+        title: getTitleFromUrl(book.url),
+        chapters
+    };
 };
 
 //This method is called by the client to get the book metadata
 const getBookMetadata = async (book) => {
-  const fileType = await fileTypeFromBuffer(book.fileContents);
-  console.log("fileTypeFromBuffer", fileType);
-  let metadata;
-  if(!fileType) {
-    //Generic metadata for other file types
-    console.log("Generic metatdata detected...");
-    metadata = await getGenericMetadata(book);
-  } else if (isEpub(fileType)) {
-    metadata = await getEpubMetadata(book);
-  } else if (isPdf(fileType)) {
-    metadata = await getPdfMetadata(book);
-  } else {
-    //Generic metadata for other file types
-    console.log("Generic metatdata detected...");
-    metadata = await getGenericMetadata(book);
-  }
-  console.log("Got book metadata", {
-    fileType: fileType,
-    title: metadata.title,
-    chapters: metadata.chapters
-  });
-  console.log("chapters", JSON.stringify(metadata.chapters));
-  return {
-    fileType: fileType,
-    title: metadata.title,
-    chapters: metadata.chapters
-  };
+    const fileType = await fileTypeFromBuffer(book.fileContents);
+    console.log("fileTypeFromBuffer", fileType);
+    let metadata;
+    if (!fileType) {
+        //Generic metadata for other file types
+        console.log("Generic metatdata detected...");
+        metadata = await getGenericMetadata(book);
+    } else if (isEpub(fileType)) {
+        metadata = await getEpubMetadata(book);
+    } else if (isPdf(fileType)) {
+        metadata = await getPdfMetadata(book);
+    } else {
+        //Generic metadata for other file types
+        console.log("Generic metatdata detected...");
+        metadata = await getGenericMetadata(book);
+    }
+    console.log("Got book metadata", {
+        fileType: fileType,
+        title: metadata.title,
+        chapters: metadata.chapters
+    });
+    console.log("chapters", JSON.stringify(metadata.chapters));
+    return {
+        fileType: fileType,
+        title: metadata.title,
+        chapters: metadata.chapters
+    };
 };
 
 export const writeMetadataToDB = async (userId, book) => {
-  const params = {
-    TableName: process.env.booksTableName,
-    Item: {
-      // The attributes of the item to be created
-      userId: userId,
-      bookId: book.id, // The book's s3 ETag
-      format: book.metadata.fileType.ext,
-      title: book.metadata.title,
-      chapters: book.metadata.chapters,
-      key: getKeyFromUrl(book.url),
-      sizeInBytes: book.fileContents.length,
-      createdAt: Date.now(), // Current Unix timestamp
-    },
-  };
+    const params = {
+        TableName: process.env.booksTableName,
+        Item: {
+            // The attributes of the item to be created
+            userId: userId,
+            bookId: book.id, // The book's s3 ETag
+            format: book.metadata.fileType.ext,
+            title: book.metadata.title,
+            chapters: book.metadata.chapters,
+            key: getKeyFromUrl(book.url),
+            sizeInBytes: book.fileContents.length,
+            createdAt: Date.now(), // Current Unix timestamp
+        },
+    };
 
-  try {
-    console.log("Writing to DB", params);
-    await dynamoDb.put(params).promise();
-  } catch (err) {
-    console.log("Problem writing to DB:", err);
-  }
+    try {
+        console.log("Writing to DB", params);
+        await dynamoDb.put(params).promise();
+    } catch (err) {
+        console.log("Problem writing to DB:", err);
+    }
 };
 
 //This method is called by the client to get the book metadata
 export const parseBookMetadata = handler(async (event) => {
-  const userId = event.requestContext.authorizer.claims.sub;
-  const body = JSON.parse(event.body);
-  const bookUrl = body.bookUrl;
+    const userId = event.requestContext.authorizer.claims.sub;
+    const body = JSON.parse(event.body);
+    const bookUrl = body.bookUrl;
 
-  const book = await getBookFromFileSystemOrS3(bookUrl);
-  await writeMetadataToDB(userId, book);
-  return {
-    statusCode: 200,
-    book: book
-  };
+    const book = await getBookFromFileSystemOrS3(bookUrl);
+    await writeMetadataToDB(userId, book);
+    return {
+        statusCode: 200,
+        book: book
+    };
 });
 
 export const onUpload = handler(async (event) => {
-  const object = event.Records[0].s3.object;
-  const key = object.key;
-  const userId = key.split("/")[0];
-  console.log("onUpload things", userId, key);
-  const book = await getBookFromFileSystemOrS3(key);
-  console.log("got book", book);
-  await writeMetadataToDB(userId, book);
-  return {
-    statusCode: 200,
-    book: book
-  };
+    const object = event.Records[0].s3.object;
+    const key = object.key;
+    const userId = key.split("/")[0];
+    console.log("onUpload things", userId, key);
+    const book = await getBookFromFileSystemOrS3(key);
+    console.log("got book", book);
+    await writeMetadataToDB(userId, book);
+    return {
+        statusCode: 200,
+        book: book
+    };
 });
 
