ff9746d1f3802298b00b238f036d2887a07b7943
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-02-06 09:27:43 -0500

    Added user table entry. Added admin

--- user.js
+++ user.js
@@ -3,14 +3,43 @@ import handler from "./libs/handler-lib";
 
 const dynamo = new AWS.DynamoDB.DocumentClient();
 
+const getOrCreateUser = async (userId, userTable) => {
+  const params = {
+    TableName: userTable,
+    Key: {
+      userId: userId
+    }
+  };
+  const user = await dynamo.get(params).promise();
+  console.log("user", user);
+  if (!user.Item) {
+    console.log("creating user", userId, "in table", userTable, "");
+    const params = {
+      TableName: userTable,
+      Item: {
+        userId: userId,
+        admin: false
+      }
+    };
+    await dynamo.put(params).promise();
+    console.log("created user", userId, "in table", userTable, "");
+    return params.Item;
+  }
+  console.log("user", userId, "already exists in table", userTable, "");
+  return user.Item;
+};
+
 export const getData = handler(async (event, context) => {
   const userId = event.requestContext.authorizer.claims.sub;
   console.log("userId", userId);
+  console.log("event.requestContext", JSON.stringify(event.requestContext));
   const stage = event.requestContext.stage;
-  const tableName = `${stage}-books`;
+  const tableNameBook = `${stage}-books`;
+  const tableNameUser = `${stage}-users`;
+  const user = await getOrCreateUser(userId, tableNameUser);
 
   const publicBooksQuery = {
-    TableName: tableName,
+    TableName: tableNameBook,
     KeyConditionExpression: "userId = :publicUser",
     ExpressionAttributeValues: {
       ":publicUser": "public"
@@ -18,7 +47,7 @@ export const getData = handler(async (event, context) => {
   };
 
   const myBooksQuery = {
-    TableName: tableName,
+    TableName: tableNameBook,
     KeyConditionExpression: "userId = :userId",
     ExpressionAttributeValues: {
       ":userId": userId
@@ -31,6 +60,7 @@ export const getData = handler(async (event, context) => {
   console.log("myBooks", myBooks);
   console.log("about to return", [...publicBooks.Items, ...myBooks.Items]);
   return {
+    user,
     books: [...publicBooks.Items, ...myBooks.Items]
   };
 });
