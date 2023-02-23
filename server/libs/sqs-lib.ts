91605b07a80e554a94322e705487622c77fd55a8
Jarad DeLorenzo <jdelorenzo@justworks.com>
2023-02-23 18:00:21 -0500

    Fixed bug with booksjobs creation

--- server/libs/sqs-lib.ts
+++ server/libs/sqs-lib.ts
@@ -1,91 +1,89 @@
 import * as AWS from "aws-sdk";
 import {
-    ISummaryFormPayload,
-    ISummaryJobPayload,
-    ISummaryJobStatus,
-    IUser,
-    JobStatus,
+  ISummaryFormPayload,
+  ISummaryJobPayload,
+  ISummaryJobStatus,
+  IUser,
+  JobStatus,
 } from "../../types/summaraizeTypes";
 
 const sqs = new AWS.SQS({
-    apiVersion: "latest",
-    region: "us-east-1",
+  apiVersion: "latest",
+  region: "us-east-1",
 });
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export const publishToSummaryQueue = async (
-    payload: ISummaryFormPayload,
-    user: IUser
+  payload: ISummaryFormPayload,
+  user: IUser
 ): Promise<ISummaryJobStatus> => {
-    const queueUrl = process.env.QUEUE_URL as string;
+  const queueUrl = process.env.QUEUE_URL as string;
 
-    const job: ISummaryJobPayload = {
-        payload,
-        userId: user.userId,
+  const job: ISummaryJobPayload = {
+    payload,
+    userId: user.userId,
+  };
+
+  const params = {
+    MessageBody: JSON.stringify(job),
+    QueueUrl: queueUrl,
+  };
+
+  const sqsResponse = await sqs.sendMessage(params).promise();
+  console.log("sqsResponse", sqsResponse);
+
+  try {
+    const jobStatus: ISummaryJobStatus = {
+      jobId: sqsResponse.MessageId as string,
+      status: JobStatus.PENDING,
+      userId: user.userId,
+      payload,
+      createdAt: new Date().toISOString(),
     };
+    console.log("jobStatus", jobStatus);
 
-    const params = {
-        MessageBody: JSON.stringify(job),
-        QueueUrl: queueUrl,
-    };
+    const a = await dynamoDb
+      .put({
+        TableName: process.env.jobsTableName as string,
+        Item: jobStatus,
+      })
+      .promise();
 
-    const sqsResponse = await sqs.sendMessage(params).promise();
-    console.log("sqsResponsey", sqsResponse);
-
-    try {
-        const jobStatus: ISummaryJobStatus = {
-            jobId: sqsResponse.MessageId as string,
-            status: JobStatus.PENDING,
-            userId: user.userId,
-            payload,
-            createdAt: new Date().toISOString(),
-        };
-        console.log("jobStatus", jobStatus);
-
-        const a = await dynamoDb
-            .put({
-                TableName: process.env.jobsTableName as string,
-                Item: jobStatus,
-            })
-            .promise();
-        console.log("a", a);
-
-        const b = await dynamoDb
-            .put({
-                TableName: process.env.booksJobsTableName as string,
-                Item: {
-                    bookId: payload.bookId,
-                    jobId: sqsResponse.MessageId,
-                }
-            });
-        console.log("b", b);
-        return jobStatus;
-    } catch (e) {
-        console.log("Error saving book job", e);
-        throw e;
-    }
-
+    const b = await dynamoDb
+      .put({
+        TableName: process.env.booksJobsTableName as string,
+        Item: {
+          bookId: payload.bookId,
+          jobId: sqsResponse.MessageId,
+        },
+      })
+      .promise();
+    return jobStatus;
+  } catch (e) {
+    console.log("Error saving book job", e);
+    throw e;
+  }
 };
 
 export const updateJobStatus = async (
-    jobId: string,
-    userId: string,
-    status: JobStatus
+  jobId: string,
+  userId: string,
+  status: JobStatus
 ) => {
-    const params = {
-        TableName: process.env.jobsTableName as string,
-        Key: {
-            jobId,
-            userId,
-        },
-        UpdateExpression: "set #status = :status",
-        ExpressionAttributeNames: {
-            "#status": "status",
-        },
-        ExpressionAttributeValues: {
-            ":status": status,
-        } as any,
-    };
-    await dynamoDb.update(params).promise();
+  const params = {
+    TableName: process.env.jobsTableName as string,
+    Key: {
+      jobId,
+      userId,
+    },
+    UpdateExpression: "set #status = :status",
+    ExpressionAttributeNames: {
+      "#status": "status",
+    },
+    ExpressionAttributeValues: {
+      ":status": status,
+    } as any,
+  };
+  await dynamoDb.update(params).promise();
 };
